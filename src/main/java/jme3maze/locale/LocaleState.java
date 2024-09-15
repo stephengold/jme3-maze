f9e85fb0bbb96a314e5efe0576279c256b3ee539
stephengold <sgold@sonic.net>
2024-09-15 14:29:40 -0700

    remove unnecessary commas from copyright notices

--- src/main/java/jme3maze/locale/LocaleState.java
+++ src/main/java/jme3maze/locale/LocaleState.java
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2015-2023, Stephen Gold
+ Copyright (c) 2015-2023 Stephen Gold
 
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions are met:
